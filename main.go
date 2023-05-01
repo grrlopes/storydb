0aeab55f53e096935009c7cbfecee0b0bcb7fa77
Gabriel Lopes <gabrielrrlopes@gmail.com>
2023-05-01 02:15:46 -0400

    Moved update and view into ui home

--- main.go
+++ main.go
@@ -4,76 +4,29 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/grrlopes/storydb/entity"
 	"github.com/grrlopes/storydb/ui"
 )
 
 const useHighPerformanceRenderer = false
 
-var (
-	command          = entity.NewCmd()
-	home    ui.IHome = ui.NewHome(command)
-)
-
 type model struct {
-	home *entity.Command
+	home ui.ModelHome
 }
 
-func (m *model) Init() tea.Cmd {
+func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		cmd  tea.Cmd
-		cmds []tea.Cmd
-	)
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
-			return m, tea.Quit
-		}
-
-	case tea.WindowSizeMsg:
-		headerHeight := lipgloss.Height(home.HeaderView())
-		footerHeight := lipgloss.Height(home.FooterView())
-		verticalMarginHeight := headerHeight + footerHeight
-
-		if !m.home.Ready {
-			m.home.Viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-			m.home.Viewport.YPosition = headerHeight
-			m.home.Viewport.HighPerformanceRendering = useHighPerformanceRenderer
-			m.home.Viewport.SetContent(m.home.Content)
-			m.home.Ready = true
-
-			m.home.Viewport.YPosition = headerHeight + 1
-		} else {
-			m.home.Viewport.Width = msg.Width
-			m.home.Viewport.Height = msg.Height - verticalMarginHeight
-		}
-		if useHighPerformanceRenderer {
-			cmds = append(cmds, viewport.Sync(m.home.Viewport))
-		}
-	}
-
-	home.WindowUpdate(m.home)
-
-	m.home.Viewport, cmd = m.home.Viewport.Update(msg)
-	cmds = append(cmds, cmd)
-	ui.NewHome(m.home)
-
-	return m, tea.Batch(cmds...)
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.home, cmd = m.home.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	if !m.home.Ready {
-		return "\n  Initializing..."
-	}
-	return fmt.Sprintf("%s\n%s\n%s", home.HeaderView(), m.home.Viewport.View(), home.FooterView())
+	return m.home.View()
 }
 
 func main() {
@@ -82,8 +35,7 @@ func main() {
 		fmt.Println("could not load file:", err)
 		os.Exit(1)
 	}
-	m := model{home: entity.NewCmd()}
-	m.home.Content = string(content)
+	m := model{home: ui.NewHome(entity.Command{Content: string(content)})}
 	p := tea.NewProgram(
 		&m,
 		tea.WithAltScreen(),
--- ui/home.go
+++ ui/home.go
@@ -1,38 +1,101 @@
 package ui
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/grrlopes/storydb/entity"
 )
+
+const useHighPerformanceRenderer = false
 
 type IHome interface {
 	HeaderView() string
 	FooterView() string
 	WindowUpdate(msg *entity.Command)
+	Update(m tea.Msg) (*entity.Command, tea.Cmd)
 }
 
-type modelHome struct {
-	home *entity.Command
+type ModelHome struct {
+	home entity.Command
 }
 
-func NewHome(m *entity.Command) IHome {
-	return &modelHome{
-		home: m,
+func NewHome(m entity.Command) ModelHome {
+	p := ModelHome{
+		home: entity.Command{
+			Content:  m.Content,
+			Cursor:   0,
+			Selected: map[int]struct{}{},
+			Ready:    false,
+			Viewport: viewport.Model{},
+		},
 	}
+	return p
 }
 
-func (m *modelHome) WindowUpdate(msg *entity.Command) {
+func (m ModelHome) WindowUpdate(msg entity.Command) {
 	m.home = msg
 }
 
-func (m modelHome) HeaderView() string {
+func (m ModelHome) HeaderView() string {
 	line := strings.Repeat("─", Max(0, m.home.Viewport.Width))
 	return lipgloss.JoinHorizontal(lipgloss.Center, line)
 }
 
-func (m modelHome) FooterView() string {
+func (m ModelHome) FooterView() string {
 	line := strings.Repeat("─", Max(0, m.home.Viewport.Width))
 	return lipgloss.JoinHorizontal(lipgloss.Center, line)
+}
+
+func (m ModelHome) Update(msg tea.Msg) (ModelHome, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
+			return m, tea.Quit
+		}
+
+	case tea.WindowSizeMsg:
+		headerHeight := lipgloss.Height(m.HeaderView())
+		footerHeight := lipgloss.Height(m.FooterView())
+		verticalMarginHeight := headerHeight + footerHeight
+
+		if !m.home.Ready {
+			m.home.Viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+			m.home.Viewport.YPosition = headerHeight
+			m.home.Viewport.HighPerformanceRendering = useHighPerformanceRenderer
+			m.home.Viewport.SetContent(m.home.Content)
+			m.home.Ready = true
+
+			m.home.Viewport.YPosition = headerHeight + 1
+		} else {
+			m.home.Viewport.Width = msg.Width
+			m.home.Viewport.Height = msg.Height - verticalMarginHeight
+		}
+		if useHighPerformanceRenderer {
+			cmds = append(cmds, viewport.Sync(m.home.Viewport))
+		}
+	}
+
+	m.WindowUpdate(m.home)
+
+	m.home.Viewport, cmd = m.home.Viewport.Update(msg)
+	cmds = append(cmds, cmd)
+	NewHome(m.home)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m ModelHome) View() string {
+	if !m.home.Ready {
+		return "\n  Initializing..."
+	}
+	return fmt.Sprintf("%s\n%s\n%s", m.HeaderView(), m.home.Viewport.View(), m.FooterView())
 }
